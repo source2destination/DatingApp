0a60604351d8edb07a21ed52142f41cda90aee56
Zac <source2destination@yahoo.com>
2019-04-30 13:34:01 -0600

    Second Initial Push

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -5,7 +5,8 @@ namespace DatingApp.API.Data
 {
     public class DataContext : DbContext
     {
-        public DbSet<Value> Values { get; set; }
         public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+        public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
